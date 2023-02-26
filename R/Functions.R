b3db15833e4a095d55286da3d4f92555846c5935
andresgmejiar <46786423+andresgmejiar@users.noreply.github.com>
2023-02-26 21:01:56 +0900

    Added getMap function
    feat: Importing rasters from a multisource environment that are projected to a desired extent and resolution is now its own function.

--- R/Functions.R
+++ R/Functions.R
@@ -576,6 +576,176 @@ getMap <- function(tiles, polys, tile_id = "TILEID", vals = "location",
   }
 }
 
+#' Import a raster for a specific region from a multisource environment, 
+#' such as the outputs of the getMap function.
+#' 
+#' @title Import a contiguous raster map
+#' @param region A SpatRaster, Raster*, SpatVector, Spatial* or character
+#' string representing the area of interest
+#' @param polys A polygon of class SpatVector representing the partitioning grid
+#' for the maximum possible area, in the same format as the output of the 
+#' \code{\link[lbmech]{makeGrid}} function. 
+#' @param tile_id A character string representing the name of the column in the 
+#' \code{polys} polygon pontaining the unique Tile IDs. Default is 
+#' \code{tile_id = 'TILEID'}
+#' @param z_fix A SpatRaster or Raster* object with the desired output projection, 
+#' resolution, and origin. Required if \code{tiles} is of classes SpatVector,
+#' Spatial*, or character, unless \code{res} is provided. 
+#' @param res The desired ouput resolution, in units of \code{region}'s projection. 
+#' Ignored if \code{z_fix} is provided or if \code{region} is of class SpatRaster
+#' or Raster*
+#' @param neighbor_distance An integer representing the number of cells that 
+#' tiles are buffered. In other words, to ensure that there are no gaps between tiles,
+#' neighboring tiles within \code{neighborhood_distance} cells are also considered as
+#' potential sources. Default is 5 cells.
+#' @param FUN Function to deal with overlapping values for overlapping tiles.
+#' Default is \code{NA}, which uses \code{\link[terra]{merge}}. To use 
+#' \code{\link[terra]{mosaic}}, provide a compatible function
+#' @param mask If \code{FALSE} (the default), the output map will contain all cells
+#' falling within the extent of \code{region}. If \code{TRUE}, places with \code{NA}
+#' (if \code{region} is SpatRaster or Raster*) or no coverage (if \code{region} is
+#' SpatVector or Spatial*) will be assigned a value of NA. 
+#' @param vals A character string or a Raster* object. Required only if the
+#' \code{z} parameter is a polygon NOT the output of the 
+#' \code{\link[lbmech]{makeGrid}} function as the default is
+#' the character string \code{'location'}. If not, the \code{vals} parameter should be
+#' set to the column name containing the URL or file path to the DEM for that
+#' sector.
+#' @param dir A filepath to the directory being used as the workspace. Default
+#' is \code{tempdir()}, but unless the analyses will only be performed a few times 
+#' it is highly recommended to define a permanent workspace.
+#' @param ... Additional agruments to pass to \code{\link[lbmech]{fix_z}}
+#' @param terra rast
+#' @param terra vect
+#' @param terra crs
+#' @param terra ext
+#' @param terra buffer
+#' @param terra intersect
+#' @param terra merge
+#' @param terra mosaic
+#' @param terra as.polygons
+#' @param terra project
+#' @param terra crs<-
+#' @param terra crop
+#' @param terra mask
+#' @examples 
+#' # Generate a DEM, export it
+#' n <- 5
+#' dem <- expand.grid(list(x = 1:(n * 100),
+#'                         y = 1:(n * 100))) / 100
+#' dem <- as.data.table(dem)
+#' dem[, z := 250 * exp(-(x - n/2)^2) + 
+#'       250 * exp(-(y - n/2)^2)]
+#' dem <- rast(dem)
+#' ext(dem) <- c(10000, 20000, 30000, 40000)
+#' crs(dem) <- "+proj=lcc +lat_1=48 +lat_2=33 +lon_0=-100 +datum=WGS84"
+#' 
+#' dir <- tempdir()
+#' writeRaster(dem, paste0(dir,"/DEM.tif"),overwrite=TRUE)
+#' 
+#' 
+#' # Import raster, get the grid
+#' dem <- rast(paste0(dir,"/DEM.tif"))
+#' grid <- makeGrid(dem = dem, nx = n, ny = n)
+#' 
+#' # Import the map for the center tile resampled to a different resolution
+#' dem2 <- importMap('SECTOR_13', grid, res = 20)
+#' @export
+importMap <- function(region, polys,
+                      tile_id = 'TILEID', z_fix = NULL, res = NULL,
+                      neighbor_distance = 5, FUN = NULL, mask = TRUE,
+                      vals = 'location',
+                      dir = tempdir(), ...){
+  dir <- normalizePath(dir,mustWork=FALSE)
+  # From the region we need the tiles that it covers, and it should be coerced
+  # to something of SpatRaster or SpatVector class
+  
+  #Try to coerce
+  if (methods::is(region,'character')){
+    region <- polys[which(unlist(polys[[tile_id]]) %in% region),]
+  } else if (methods::is(region,'Raster')){
+    region <- rast(region)
+  } else if (methods::is(region,'Spatial')){
+    region <- vect(region)
+  }
+  if (methods::is(z_fix,'Raster')){
+    z_fix <- rast(z_fix)
+  }
+  
+  # Deal with terra objects
+  if (is.null(z_fix) & !methods::is(region,'SpatRaster')){
+    z_fix <- fix_z(crs(region), res = res)
+  }
+  if (methods::is(region,'SpatRaster')){
+    if (is.null(z_fix)){
+      z_fix <- region
+    }
+  } 
+  
+  # Get the tile names, make sure maps have been downlaoded
+  tiles <- buffer(region,width=neighbor_distance)
+  tiles <- intersect(region[,NA],polys)[[tile_id]]
+  tiles <- unlist(tiles)
+  getMap(tiles, polys, tile_id = 'TILEID', dir = dir, ...)
+  
+  # Import all DEMs
+  dem <- normalizePath(paste0(dir,'/',tiles),mustWork=FALSE)
+  dem <- lapply(dem,importRST)
+  dem <- suppressWarnings(lapply(dem,
+                                 project,
+                                 y = dem[[1]],
+                                 align = TRUE))
+  
+  # Merge or mosaic them, depending on whether a FUN is provided 
+  if (length(dem) > 1){
+    if (is.null(FUN)){
+      dem <- do.call(merge,dem)
+    } else {
+      dem$fun <- FUN
+      dem <- do.call(mosaic,dem)
+    }
+  } else {
+    dem <- dem[[1]]
+  }
+  
+  # Create a cropping polygon. This is equivalent to the extent of the
+  # region shape, expanded in the X and Y directions by the
+  # pixel size times the connectivity contiguity. This has to be
+  # reprojected back to the original coordinate system since only it
+  # preserves orthogonality. This step saves us from having to initially project
+  # a rather large SpatRaster
+  if (methods::is(region,'SpatRaster')){
+    poly <- region
+    poly[poly == poly] <- 1
+    poly <- as.polygons(poly)
+  } else {
+    poly <- region
+  }
+  
+  poly <- project(poly,dem)
+  poly <- unlist(ext(poly))
+  poly <- c(poly[1] - (neighbor_distance + 1) * res(z_fix)[1], poly[2] + 
+              (neighbor_distance + 1) * res(z_fix)[1],
+            poly[3] - (neighbor_distance + 1) * res(z_fix)[2], poly[4] + 
+              (neighbor_distance + 1) * res(z_fix)[2])
+  poly <- ext(poly)
+  poly <- as.polygons(poly)
+  crs(poly) <- crs(dem)
+  
+  # Crop the mosaiced raster by the cropping polygon, project it
+  dem <- crop(dem,poly)
+  dem <- suppressWarnings(project(dem,z_fix,align = TRUE))
+  
+  # Should the output raster have the same extent, or the same coverage?
+  if (mask == TRUE){
+    dem <- mask(dem,region)
+  }
+  
+  if (!all.equal(crs(z_fix), crs(region))){
+    warning("Different projections provided for 'z_fix' and 'region'. Output is the same as 'z_fix'")
+  }
+  return(dem)
+}
 
 #' Import \code{GPX} tracks from commercial GPS equipment as a data.table
 #' ready for velocity function estimation. Note that the output coordinates
@@ -985,17 +1155,15 @@ getVelocity <- function(data, x = 'x', y ='y', degs = FALSE, dl = NULL, z = 'z',
     data_points <- as.data.table(intersect(data_points,z))
     tiles <- unique(data_points[,get(tile_id)])
     
-    # Make sure that the necessary files exist
-    getMap(tiles=tiles,polys=z,tile_id=tile_id,vals=vals,
-           dir= normalizePath(paste0(dir,"/Elevations/")))
-    
     # Create an empty data.table in which to store extracted values. There's a
     # small chance some points will land in two sectors, so we'll have
     # to collect all and consolidate
     data_new <- data.table()
     for (tile in tiles){
-      elevs <- importRST(normalizePath(paste0(rd,"/",tile),mustWork=FALSE))
-      
+      elevs <- importMap(tile, polys = z, tile_id = tile_id, vals = vals,
+                         dir = normalizePath(paste0(dir,"/Elevations/")),
+                         neighbor_distance = 0)
+
       d <- data_points[get(tile_id) == tile]
       z_vals <- elevs[cellFromXY(elevs,d[,.(get(..x),get(..y))])]
       d[,z := ..z_vals]
@@ -1389,27 +1557,11 @@ makeWorld <- function(tiles,polys,cut_slope,tile_id = 'TILEID', proj = crs(polys
       poly_buff <- buffer(poly,width=neighbor_distance)
       mzip <- intersect(poly_buff[,NA],polys)[[tile_id]]
       mzip <- unlist(mzip)
-      getMap(mzip, polys = polys, tile_id = tile_id, vals = vals,dir = 
-               normalizePath(paste0(dir,"/Elevations/")))
       
-      # Import all DEMs
-      dem <- normalizePath(paste0(dir,"/Elevations/",mzip),mustWork=FALSE)
-      dem <- lapply(dem,importRST)
-      dem <- suppressWarnings(lapply(dem,
-                                     project,
-                                     y = dem[[1]],
-                                     align = TRUE))
-      
-      if (length(dem) > 1){
-        if (is.null(FUN)){
-        dem <- do.call(merge,dem)
-        } else {
-        dem$fun <- FUN
-        dem <- do.call(mosaic,dem)
-      }
-        } else {
-        dem <- dem[[1]]
-      }
+      dem <- importMap(mzip, polys = polys, tile_id = tile_id, vals = vals,
+                       z_fix = z_fix, 
+                       dir = normalizePath(paste0(dir,"/Elevations/")))
+
       
       # Fix the units if they weren't provided in meters
       if (unit == "km"){
@@ -1419,24 +1571,7 @@ makeWorld <- function(tiles,polys,cut_slope,tile_id = 'TILEID', proj = crs(polys
       } else if (unit == "mi"){
         dem <- dem*5280*12*2.54/100
       }
-      
-      # Create a cropping polygon. This is equivalent to the extent of the
-      # tile shape, expanded in the X and Y directions by the
-      # pixel size times the connectivity contiguity. This has to be
-      # reprojected back to the original coordinate system since only it
-      # preserves orthogonality
-      poly <- project(poly,dem)
-      poly <- unlist(ext(poly))
-      poly <- c(poly[1] - (contiguity + 1) * l_p[1], poly[2] + (contiguity + 1) * l_p[1],
-                poly[3] - (contiguity + 1) * l_p[2], poly[4] + (contiguity + 1) * l_p[2])
-      poly <- ext(poly)
-      poly <- as.polygons(poly)
-      crs(poly) <- crs(dem)
-      
-      # Crop the mosaiced raster by the cropping polygon, project it
-      dem <- crop(dem,poly)
-      dem <- suppressWarnings(project(dem,z_fix,align = TRUE))
-      
+
       name <- i
       
       nas <- cells(dem)
