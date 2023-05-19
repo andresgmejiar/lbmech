c82d31e72cc1d2f6192ab5e5ce51c646d2198d15
andresgmejiar <46786423+andresgmejiar@users.noreply.github.com>
2023-05-19 18:05:36 +0900

    Use of matrixStats for weightedMean
    perf: matrixStats is about 3x faster for larger datasets. Could then just remove average() function but it's useful to be able to shift which type of average based solely on a string

--- R/average.R
+++ R/average.R
@@ -4,8 +4,8 @@
 #' @param x A numeric vector with the values to be averaged.
 #' @param w A vector of weights of the same length as \code{x}.
 #' @param type A character string, either: 'mean' or 'L2' (the default),
-#' such that a weighted mean is computed; or 'median' or 'L1', such
-#' that a weighted median is computed using \code{\link[DescTools]{Median}}.
+#' such that a weighted mean is computed  using \code{\link[matrixStats]{weightedMean}}; or 'median' or 'L1', such
+#' that a weighted median is computed using \code{\link[matrixStats]{weightedMedian}}.
 #' @param na.rm Should \code{NA} values be ignored? Default is \code{na.rm = FALSE}.
 #' @return A numeric value, representing the weighted mean or median
 #' @examples 
@@ -22,7 +22,7 @@
 #' @export
 average <- function(x, w = rep(1,length(x)), type = 'mean',na.rm = FALSE){
   if (type == 'mean' | type == 'L2'){
-    return(sum(x * w/sum(w,na.rm=na.rm), na.rm=na.rm))
+    return(matrixStats::weightedMean(x, w = w, na.rm = na.rm))
   } else if (type == 'median' | type == 'L1'){
   return(matrixStats::weightedMedian(x, w = w, na.rm = na.rm))
   }
